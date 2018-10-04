ac4d97978a4403e473ee1d1600c501852dcfa498
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-10-04 13:24:01 +0200

    t

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Card from './Card'
+import GuessCount from './GuessCount'
 
 class App extends Component {
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
+
   render() {
+    const won = new Date().getSeconds() % 2 === 0
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <p>GAGNÉ !</p>}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
